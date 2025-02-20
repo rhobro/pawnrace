f9943a84153a02f64aebe971e4b95a9b0ea76530
Rohan Mathew <robozmat@gmail.com>
2025-02-20 03:44:45 +0000

    ping pong working

--- logic/src/lib.rs
+++ logic/src/lib.rs
@@ -1,12 +1,66 @@
-use jni::{objects::{JClass, JObject, JString}, sys::{jboolean, jchar, jint}, JNIEnv};
+use jni::{
+    objects::{JClass, JObject, JString, JValue},
+    JNIEnv,
+};
 
 #[no_mangle]
-pub extern "system" fn Java_pawnrace_PawnRace_play<'local>(
-    mut env: JNIEnv<'local>,
-    class: JClass<'local>,
-    colour: JString<'local>,
-    output: JObject<'local>,
-    input: JObject<'local>
+pub extern "system" fn Java_pawnrace_PawnRace_play<'a>(
+    mut env: JNIEnv<'a>,
+    _: JClass<'a>,
+    colour: JString<'a>,
+    output: JObject<'a>,
+    input: JObject<'a>,
 ) {
-    
+    // process args
+    let colour: String = env
+        .get_string(&colour)
+        .expect("Couldn't read JNI colour input")
+        .into();
+    let mut io = IO::new(input, output, env);
+
+    let test = io.recv();
+    io.send(test);
+}
+
+struct IO<'a> {
+    input: JObject<'a>,
+    output: JObject<'a>,
+    env: JNIEnv<'a>,
+}
+
+impl<'a> IO<'a> {
+    fn recv(&mut self) -> String {
+        // call
+        let js: JString = self
+            .env
+            .call_method(&self.input, "readLine", "()Ljava/lang/String;", &[])
+            .unwrap()
+            .l()
+            .unwrap()
+            .into();
+
+        // convert string
+        let s: String = self.env.get_string(&js).unwrap().into();
+
+        s
+    }
+
+    fn send(&mut self, s: impl AsRef<str>) {
+        // convert string
+        let js = self.env.new_string(s).unwrap();
+
+        // call
+        self.env
+            .call_method(
+                &self.output,
+                "println",
+                "(Ljava/lang/String;)V",
+                &[JValue::Object(&js)],
+            )
+            .unwrap();
+    }
+
+    fn new(input: JObject<'a>, output: JObject<'a>, env: JNIEnv<'a>) -> IO<'a> {
+        Self { input, output, env }
+    }
 }
